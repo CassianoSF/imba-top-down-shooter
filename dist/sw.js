7440be56155281096c781c1afbef4ef2594aa493
CassianoSF <cassianosfranco@gmail.com>
2019-07-03 11:59:42 -0300

    cache log

--- dist/sw.js
+++ dist/sw.js
@@ -525,16 +525,7 @@ self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+      return request || fetch(e.request)
     })
   )
 })
